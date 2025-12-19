19243eabfb4fd9747af2a0857d10653c662322a3
Daniel Agota <20474348+fixbits@users.noreply.github.com>
2025-12-19 20:42:48 +0100

    Tests for basic terms, predicates, formulas

--- src/main/scala/logic/Formula.scala
+++ src/main/scala/logic/Formula.scala
@@ -9,8 +9,8 @@ enum Formula[+A]:
     case Or(p: Formula[A], q: Formula[A])
     case Imp(p: Formula[A], q: Formula[A])
     case Iff(p: Formula[A], q: Formula[A])
-    case Forall(x: Formula[A], p: Formula[A])
-    case Exists(x: Formula[A], p: Formula[A])
+    case Forall(x: String, p: Formula[A])
+    case Exists(x: String, p: Formula[A])
 
 object Formula:
     /** Smart constructors (OCaml needs these because it can't use constructors 
@@ -26,8 +26,8 @@ object Formula:
 
     def mkIff[A](p: Formula[A], q: Formula[A]): Formula[A] = Iff(p, q)
 
-    def mkForall[A](x: Formula[A], p: Formula[A]): Formula[A] = Forall(x, p)
-    def mkExists[A](x: Formula[A], p: Formula[A]): Formula[A] = Exists(x, p)
+    def mkForall[A](x: String, p: Formula[A]): Formula[A] = Forall(x, p)
+    def mkExists[A](x: String, p: Formula[A]): Formula[A] = Exists(x, p)
 
     /** Convenience constructors for common cases to stay consistent */
     def atom[A](a: A): Formula[A] = Atom(a)
